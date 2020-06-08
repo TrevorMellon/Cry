23b6fbb9d41fa79af24da6a6f643e0334eaaa44b
Codified <codified@protonmail.com>
2020-06-08 13:32:56 +0100

    add boost

--- cmake/req.cmake
+++ cmake/req.cmake
@@ -1,4 +1,16 @@
 
 set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CMAKE_SOURCE_DIR}/cmake/modules/")
 
-find_package(gcrypt)
+find_package(gcrypt REQUIRED)
+
+find_package(Boost REQUIRED filesystem system program_options)
+
+SET ( LIBS
+    ${GCRYPT_LIBRARIES}
+    ${Boost_LIBRARIES}
+)
+
+SET (INCS
+    ${GCRYPT_INCLUDE_DIR}
+    ${Boost_INCLUDE_DIR}
+)
