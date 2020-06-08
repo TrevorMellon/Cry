a18d29a347c16a34d5baa2662ea00d97f15f8102
Codified <codified@protonmail.com>
2020-06-08 13:20:09 +0100

    src.cmake

---
+++ cmake/src.cmake
@@ -0,0 +1,11 @@
+
+SET ( SRC_T
+    ${CMAKE_SOURCE_DIR}/src/main.cpp
+    ${CMAKE_SOURCE_DIR}/src/application.cpp
+)
+
+SET ( INC_T
+    ${CMAKE_SOURCE_DIR}/include/application.h
+)
+
+SET ( SRC ${SRC_T}) 
