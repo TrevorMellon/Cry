0ba9386cdfb9bb95f38ade5d24ca36b2bfef0bc5
Codified <codified@protonmail.com>
2020-06-08 13:34:44 +0100

    boost: add date_time

--- cmake/req.cmake
+++ cmake/req.cmake
@@ -3,7 +3,7 @@ set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CMAKE_SOURCE_DIR}/cmake/modules/")
 
 find_package(gcrypt REQUIRED)
 
-find_package(Boost REQUIRED filesystem system program_options)
+find_package(Boost REQUIRED filesystem system program_options date_time)
 
 SET ( LIBS
     ${GCRYPT_LIBRARIES}
