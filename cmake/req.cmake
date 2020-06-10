10c022e39cb9b3486dd86ec592763d5adbb2d5af
Trevor Mellon <codified@protonmail.com>
2020-06-10 20:07:11 +0100

    set boost to all dynamic on windows

--- cmake/req.cmake
+++ cmake/req.cmake
@@ -3,6 +3,10 @@ set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CMAKE_SOURCE_DIR}/cmake/modules/")
 
 find_package(gcrypt REQUIRED)
 
+if(WIN32)
+add_definitions(-DBOOST_ALL_NO_LIB -DBOOST_ALL_DYN_LINK=1)
+endif(WIN32)
+
 find_package(Boost REQUIRED filesystem system program_options date_time)
 
 SET ( LIBS
