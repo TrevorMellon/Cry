0ed3935fe32db297c438b06a89767f074a043ece
Codified <codified@protonmail.com>
2020-06-15 11:39:48 +0100

    #5 add conditional parameters in a config file

--- src/application.cpp
+++ src/application.cpp
@@ -11,12 +11,9 @@
 #include <boost/program_options.hpp>
 #include <boost/filesystem.hpp>
 
-#define GPG_ALGO GCRY_CIPHER_SERPENT256
+#include <config.h>
 
-#define CRY_EXT ".cry"
-#define CRY_DEFAULT_PASSWORD "ozmodus9"
-
-const char* defualt_crypt_string = "cRy-b@by-369";
+const char* defualt_crypt_string = CRY_DEFAULT_IV;
 
 namespace po = boost::program_options;
 namespace fs = boost::filesystem;
