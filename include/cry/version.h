7eabf02a3979f636d9da8ade969cbbc3644aa63f
Codified <codified@protonmail.com>
2020-06-16 14:55:15 +0100

    change version.h path in config.h.in

--- include/cry/version.h
+++ include/cry/version.h
@@ -3,7 +3,7 @@
 
 #define CRY_VERSION_MAJOR 0)
 #define CRY_VERSION_MINOR 1)
-#define CRY_VERSION_BUILD 6)
-#define CRY_VERSION_STRING "0.1.6")
+#define CRY_VERSION_BUILD 7)
+#define CRY_VERSION_STRING "0.1.7")
 
 #endif //CRY_VERSION_H
