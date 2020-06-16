c51a91aa17c888e5e72ee2085fcef1acb3255659
Codified <codified@protonmail.com>
2020-06-16 14:40:37 +0100

    include the version header

---
+++ include/cry/version.h
@@ -0,0 +1,9 @@
+#ifndef CRY_VERSION_H
+#define CRY_VERSION_H
+
+#define CRY_VERSION_MAJOR 0)
+#define CRY_VERSION_MINOR 1)
+#define CRY_VERSION_BUILD 6)
+#define CRY_VERSION_STRING "0.1.6")
+
+#endif //CRY_VERSION_H
