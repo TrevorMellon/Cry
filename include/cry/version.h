9c3c007e9adbfeb1597db6a01de7e17985376b0e
Codified <codified@protonmail.com>
2020-11-08 13:49:37 +0000

    Remove Version.h

--- include/cry/version.h
+++
@@ -1,9 +0,0 @@
-#ifndef CRY_VERSION_H
-#define CRY_VERSION_H
-
-#define CRY_VERSION_MAJOR 0)
-#define CRY_VERSION_MINOR 2)
-#define CRY_VERSION_BUILD 35)
-#define CRY_VERSION_STRING "0.2.35")
-
-#endif //CRY_VERSION_H
